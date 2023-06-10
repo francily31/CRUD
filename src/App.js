f564ee3cca101a6577a5853ce21d15dc1d9da91c
francily31 <francielysilva.31@hotmail.com>
2023-06-09 23:11:08 -0300

    Atualização do JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as  Router, Switch, Route, Link} from 'react-router-dom' 
+import  {Home}  from './components/pages/Home';
+import Contact from './components/pages/Contact'
+import Company from './components/pages/Company'
+import NewProject from './components/pages/NewProject'
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello my friend!   
-          </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //aqui é o JSX, dentro dele eu faço as alterações para aparecer na parte de front-end/visual do usuário
+   <Router>
+    {/**parte fisica do jsx */}
+    <ul>
+      <Link to="/">Home</Link>
+      <Link to="/contact">Contato</Link>
+      <Link to="/company">Empresa</Link>
+      <Link to="/newproject">Novo Projeto</Link>
+    </ul>
+
+    {/**alterar as paginas do projeto */}
+    <Switch>
+
+      {/**home do projeto */}
+      <Route exact path='/'>
+        <Home />
+      </Route>
+
+      <Route exact path='/contact'>
+        <Contact />
+      </Route>
+
+      <Route exact path='/newproject'>
+        <NewProject />
+      </Route>
+
+    </Switch>
+
+    <p>footer</p>
+
+   </Router>
   );
 }
 
